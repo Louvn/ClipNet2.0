8a8e458c74e6302c7ab228af65976ced1817718a
Marlon Rauschen <m.rauschen11@gmail.com>
2026-06-02 22:06:38 +0200

    eager loading for article search

--- backend/core/search/search_articles.py
+++ backend/core/search/search_articles.py
@@ -1,23 +1,31 @@
-from ...models import Revision
+from ...models import Revision, Article
 from ...database import get_db
 from fastapi import Depends
 from sqlalchemy import or_
+from sqlalchemy.orm import selectinload
 
 def search_articles(query, filters, db):
         
     # search revisions
-    revisions = db.query(Revision).filter(
-        or_(
-            Revision.title.ilike(f"%{query}%"),
-            Revision.content.ilike(f"%{query}%")
-        )
-    ).all()
-
-    # ignore revisions that are not current revision of an article
-    revisions = [revision for revision in revisions if revision.article.current_revision == revision]
+    revisions = (
+        db.query(Revision)
+            .options(
+                selectinload(Revision.article).selectinload(Article.current_revision),
+                selectinload(Revision.article).selectinload(Article.first_revision),
+                selectinload(Revision.article).selectinload(Article.op),
+                selectinload(Revision.user)
+            )
+            .join(Revision.article)
+            .filter(Revision.id == Article.current_revision_id)
+            .filter(
+                or_(
+                    Revision.title.ilike(f"%{query}%"),
+                    Revision.content.ilike(f"%{query}%")
+                )
+            ).all()
+    )
 
     # get the articles from the revisions
     articles = [revision.article for revision in revisions]
-
 
     return articles
